2c0fb0a02ba6b15017ed862e8cbc46f323b11f8a
Bohdan Domashenko <got.bogdan@gmail.com>
2024-09-23 18:58:03 +0300

    add search engine

--- backend/src/modules/paper/paper.service.ts
+++ backend/src/modules/paper/paper.service.ts
@@ -3,6 +3,7 @@ import {
   Injectable,
   InternalServerErrorException,
 } from '@nestjs/common';
+import Fuse from 'fuse.js';
 import { PrismaService } from '../database/prisma.service';
 import { PaperParserService } from '../paper-parser/paper-parser.service';
 import { PubmedService } from '../pubmed/pubmed.service';
@@ -80,13 +81,30 @@ export class PaperService {
 
         try {
           const data = await this.conductscienceSdkService.products.getByTitle({
-            title: item.material.replace(/-/g, ' '),
+            title: item.material,
           });
           if (data?.length) {
-            url = data[0]?.link;
+            const options = {
+              includeScore: true,
+              isCaseSensitive: false,
+              // Search in `author` and in `tags` array
+              keys: ['title'],
+            };
+
+            const fuse = new Fuse(data, options);
+
+            const result = fuse.search(item.material.replace(/-/g, ' '));
+
+            const relevantItem =
+              result[0]?.score < 0.3 ? result[0]?.item : null;
+
+            console.log(123, item.material, result);
+
+            url = relevantItem?.link;
           }
         } catch (error) {
-          console.error(error);
+          if (!error) console.error('Error fetching product');
+          // console.error(error);
         }
 
         items.push({
